f1c3a7ec34c62be9d6e6f1eb3ae2c122d19368f5
kumarSonal308 <121526717+kumarSonal308@users.noreply.github.com>
2023-07-04 16:38:10 +0530

    This is an clone of MI-STORE official website

---
+++ src/Components/NavOptios.js
@@ -0,0 +1,98 @@
+import React,{useState,useEffect} from 'react'
+import NavCard from "./NavCard.js"
+import "../styles/NavOptios.css"
+
+
+
+
+const NavOptios = ( {miPhones,redmiPhones,tv,laptop,fitnessAndLifeStyle,home,audio,accessories}) => {
+
+   const [miPhoneToggle, setMiPhoneToggle] = useState(false);
+   const [redmiPhoneToggle,setRedmiPhoneToggle] = useState(false);
+   const [tvToggle,setTvToggle] = useState(false);
+   const [laptopToggle,setLaptopToggle] = useState(false);
+   const [fitnessToggle,setFitnessToggle] = useState(false);
+   const [homeToggle,setHomeToggle] = useState(false);
+   const [audioToggle,setAudioToggle] = useState(false);
+   const [accessoriesToggle,setAccessoriesToggle] = useState(false);
+ 
+
+   useEffect(() => {
+    const path = window.location.pathname;
+  
+    if (path === "/miphones") {
+      setMiPhoneToggle(true);
+    } else if (path === "/redmiphones") {
+      setRedmiPhoneToggle(true);
+    } else if (path === "/tv") {
+      setTvToggle(true);
+    } else if (path === "/laptops") {
+      setLaptopToggle(true);
+    } else if (path === "/lifestyle") {
+      setFitnessToggle(true);
+    } else if (path === "/home") {
+      setHomeToggle(true);
+    } else if (path === "/audio") {
+      setAudioToggle(true);
+    } else if (path === "/accessories") {
+      setAccessoriesToggle(true);
+    }
+  }, []);
+  
+
+    return (
+        <div className="navOptions"> 
+
+            {(miPhoneToggle===true)?   miPhones.map((item)=>(
+
+                 < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+              )) : null }
+            
+      
+            {redmiPhoneToggle?   redmiPhones.map((item)=>(
+
+               < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+           )) : null }
+     
+
+            {tvToggle ?   tv.map((item)=>(
+
+                < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+           )) : null }
+
+           {laptopToggle ?   laptop.map((item)=>(
+
+              < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+           )) : null }
+     
+   
+
+            {fitnessToggle ?   fitnessAndLifeStyle.map((item)=>(
+
+              < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+           )) : null }
+     
+
+               {homeToggle ?   home.map((item)=>(
+
+                   < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+              )) : null }
+     
+     
+
+            {audioToggle ?   audio.map((item)=>(
+
+             < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+            )) : null }
+     
+
+            {accessoriesToggle ?   accessories.map((item)=>(
+
+                < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+             )) : null }
+     
+        </div>
+    )
+}
+
+export default NavOptios
